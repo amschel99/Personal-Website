e41e99beb75ed6e650003d9daa76b769f2370923
amschel99 <6290.2020@students.ku.ac.ke>
2022-09-01 21:36:31 +0300

    changed subscribe to popup

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -61,8 +61,43 @@ I also talk about Startups,Entrepreneurship and Business in my <span><a href="ht
             </Tilt>
           </Col>
         </Row>
+
+
+         <Row>
+          <button data-tf-popup="LEXdgmKc" data-tf-iframe-props="title=Subscribe" data-tf-medium="snippet"
+        style={{
+           all:'unset','font-family':'Helvetica,Arial,sans-serif',
+           display:'inline-block',
+           'max-width':'100%',
+           'white-space':'nowrap',
+           'overflow':'hidden',
+           'text-overflow':'ellipsis',
+           'background-color':'#031737',
+
+           'color':'#FFFFFF',
+           'font-size':'20px',
+           'border-radius':'25px',
+           'padding':'0 33px',
+           'font-weight':'bold',
+
+           'height':'50px',
+           'cursor':'pointer',
+           'line-height':'50px',
+
+           'text-align':'center',
+           'margin':'0',
+           'text-decoration':'none',
+
+
+        }}
+        
+      >SUBSCRIBE
+        TO NEWSLETTER</button>
+        
+      <script src="//embed.typeform.com/next/embed.js"></script>
+         </Row>
         <Row>
-         
+        
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
