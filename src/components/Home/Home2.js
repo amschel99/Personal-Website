b20c937a8d4687ec3049fbc8b630c0fc2a4642c3
amschel99 <6290.2020@students.ku.ac.ke>
2022-09-04 13:08:09 +0300

    changed to revue

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -60,7 +60,18 @@ I also talk about Startups, Entrepreneurship and Business in my
         </Row>
 
          
-<Widget id="LEXdgmKc" style={{ width: '100%' ,height:'300px'}} className="my-form" />
+ <Form action="https://www.getrevue.co/profile/amscheltech/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+    
+      </Form.Group>
+
+      
+      <Button variant="primary" type="submit">
+      Subscribe
+      </Button>
+    </Form>
         
     
        
