9d6bef08af2ef3d61eb54a1085a7d3f0cc08dffa
amschel <102370012+amschel99@users.noreply.github.com>
2023-08-21 11:47:21 -0700

    Update singleBlog.js to add a better ui

--- src/components/singleBlog.js
+++ src/components/singleBlog.js
@@ -6,7 +6,7 @@ import {marked} from 'marked';
 import DOMPurify from 'dompurify';
 import ReactMarkdown from 'react-markdown';
 import { Container, Row, Col,Form,Button } from "react-bootstrap";
-import Pre from './Pre';
+
 // Set options for marked library
 
 const SINGLE_POST= gql`
@@ -28,6 +28,7 @@ const SingleBlog = () => {
 
 
   const { id } = useParams();
+  console.log(id)
 
   const { loading, error, data } = useQuery(SINGLE_POST, {
     variables: { slug:id, hostname: "amschel" },
@@ -38,109 +39,49 @@ const SingleBlog = () => {
   }
 
   if (loading) {
-      return <div id="preloader" ></div>
+      return <div  >still loading...</div>
   }
 
   if (data) {
  
 
     return (
-      <div style={{ marginTop: '100px' }}>
+      <div style={{ marginTop: '0px' }}>
         <Card
           style={{
-            backgroundColor: 'rgba(12, 8, 24, 0.904)',
+            backgroundColor: 'white',
             border: 'none',
-            borderRadius: '2rem',
-            boxShadow: '0 0 1rem rgba(255, 255, 255, 0.3)',
+            marginBottom:"20px"
+           
           }}
         >
           <Card.Body>
             <Card.Title
               style={{
-                color: '#F6D186',
+                color: 'black',
                 fontFamily: 'Carter One, sans-serif',
-                fontSize: '3rem',
+                fontSize: '1.5rem',
                 fontWeight: 'bold',
                 marginBottom: '2rem',
-                textShadow: '2px 2px 0px rgba(255, 255, 255, 0.5)',
+        textAlign:"center"
               }}
             >
               {data.post.title}
             </Card.Title>
           
-<div style={{ color: "white" }} dangerouslySetInnerHTML={{ __html: data.post.content }}/>
+<div style={{ color: "black",margin:"15px" }} dangerouslySetInnerHTML={{ __html: data.post.content }}/>
 
 
 
 
 
           </Card.Body>
-          <Card.Footer
-            style={{
-              backgroundColor: '#112020',
-              borderTop: 'none',
-              borderRadius: '0 0 2rem 2rem',
-              color: '#F6D186',
-              fontFamily: 'Carter One, sans-serif',
-              fontSize: '1.5rem',
-              opacity: '0.7',
-              padding: '2rem',
-              textAlign: 'center',
-              textShadow: '1px 1px 0px rgba(255, 255, 255, 0.5)',
-            }}
-          >
+      
             
-               
- <Row style={{marginBottom:'10px'}}>
-  <h5>Subscribe to my news letter to never miss out on any useful article I publish</h5>
- <div id="mc_embed_signup" style={{ background: "rgba(12, 8, 24, 0.904)", color: "#fff", padding: "20px", width:'100%' }}>
-  <form
-    action="https://tech.us9.list-manage.com/subscribe/post?u=19ab3433c4d8fab33df9052d2&amp;id=3af49055ee&amp;f_id=002f22e1f0"
-    method="post"
-    id="mc-embedded-subscribe-form"
-    name="mc-embedded-subscribe-form"
-    className="validate"
-    target="_blank"
-    noValidate
-  >
-    <div id="mc_embed_signup_scroll">
-      <div className="mc-field-group" style={{ marginBottom: "10px" }}>
-        <input
-          type="email"
-          defaultValue=""
-          placeholder="Enter your email adress"
-          name="EMAIL"
-          className="required email"
-          id="mce-EMAIL"
-          required
-          style={{  '::placeholder': { 
-      color: '#00ffea' 
-    } ,color:"#00ffea", padding: "10px", marginRight: "20px", borderRadius: "5px", border: "none", background: "#555"}}
-        />
-        <input
-          type="submit"
-          value="Subscribe"
-          name="subscribe"
-          id="mc-embedded-subscribe"
-          className="button"
-          style={{borderRadius: "5px", border: "none", background: "black", color: "#fff", cursor: "pointer" , marginTop:"15px", fontSize:"20px", height:'50px'}}
-        />
-      </div>
-    </div>
-  </form>
-</div>
 
-
- </Row>
- <small style={{ fontSize: '1rem' }}>
-              {new Date(data.post.dateAdded).toLocaleDateString()}
-            </small>
-            <span className="mx-2" style={{ color: '#F6D186' }}>
-              
-            </span>
-            <small style={{ color: '#F6D186' }}>{data.post.slug}</small>
-          </Card.Footer>
+<div style={{height:"10px"}}></div>
         </Card>
+
       </div>
     );
   }
