599d14d54c2bc54d9daca4155a4e950e943bdd2f
amschel99 <6290.2020@students.ku.ac.ke>
2023-08-21 22:08:30 -0700

    did some content updates

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,50 +1,53 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
 import {
+  DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiGit,
   DiPython,
-  DiDocker,
-  DiRust,
-  DiEthereum,
-  DiTypescript,
+  DiGit,
+  
 } from "react-icons/di";
-import { SiSolana } from "react-icons/si";
+import {
+  SiPytorch,
+  SiFirebase,
+  SiNextdotjs,
+} from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiRust />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolana />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiEthereum />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiTypescript />
+        <SiPytorch />
       </Col>
     </Row>
   );
