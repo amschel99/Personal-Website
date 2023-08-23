e81a4c6c42748fc5a8c23b190400dcde6eb2d728
amschel <102370012+amschel99@users.noreply.github.com>
2023-08-23 12:03:54 -0700

    Update singleBlog.js

--- src/components/singleBlog.js
+++ src/components/singleBlog.js
@@ -7,22 +7,6 @@ import DOMPurify from 'dompurify';
 import ReactMarkdown from 'react-markdown';
 import { Container, Row, Col,Form,Button } from "react-bootstrap";
 
-function applyCodeBlockStyles(htmlContent) {
-  // Create a DOM element to parse the HTML
-  const tempElement = document.createElement("div");
-  tempElement.innerHTML = htmlContent;
-
-  // Find all <code> and <pre> elements within the parsed HTML
-  const codeBlocks = tempElement.querySelectorAll("code, pre");
-
-  // Apply styles to each code block
-  codeBlocks.forEach((codeBlock) => {
-    codeBlock.style.backgroundColor = "#333"; // Dark background color
-    codeBlock.style.color = "white"; // Text color for code
-    codeBlock.style.padding = "10px"; // Optional padding for code blocks
-    codeBlock.style.borderRadius = "5px"; // Optional border radius for rounded corners
-    codeBlock.style.overflowX = "auto"; // Add horizontal scroll if code overflows
-  });
 
 const SINGLE_POST= gql`
 query($slug:String!, $hostname:String!){
@@ -59,7 +43,7 @@ const SingleBlog = () => {
 
   if (data) {
  
-  const modifiedContent = applyCodeBlockStyles(data.post.content);
+  
     return (
       <div style={{ marginTop: '50px' }}>
         <Card
@@ -84,7 +68,7 @@ const SingleBlog = () => {
               {data.post.title}
             </Card.Title>
           
-<div style={{ color: "black",margin:"15px" }} dangerouslySetInnerHTML={{ __html: modifiedContent }}/>
+<div style={{ color: "black",margin:"15px" }} dangerouslySetInnerHTML={{ __html:data.post.content}}/>
 
 
 
